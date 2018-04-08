5fff4b66c0e9c103094f73225f751b291c81de4e
Thomas Hodge <thodge@Thomass-MacBook-Pro.local>
2018-04-08 16:31:52 -0600

    first commit

---
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 31/8/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var restaurant: Restaurant = Restaurant()
+    
+    // MARK: - View controller life style
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
