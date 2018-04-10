d3f3281e1ef54baeb2fefea5c1f6ca37dc840d5d
Thomas Hodge <thodge@Thomass-MacBook-Pro.local>
2018-04-09 22:18:26 -0600

    built out the detail header view, made custom settings for the elements

---
+++ FoodPin/View/RestsaurantDetailHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  RestsaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Thomas Hodge on 4/9/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestsaurantDetailHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
