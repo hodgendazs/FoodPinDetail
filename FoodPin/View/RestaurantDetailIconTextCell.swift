659d739876df8be78ee888850c809c37543bcff6
Thomas Hodge <thodge@Thomass-MacBook-Pro.local>
2018-04-11 17:36:42 -0600

    added two table views

---
+++ FoodPin/View/RestaurantDetailIconTextCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantDetailIconTextCell.swift
+//  FoodPin
+//
+//  Created by Thomas Hodge on 4/10/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailIconTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodPin/View/RestaurantDetailTextCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantDetailTextCell.swift
+//  FoodPin
+//
+//  Created by Thomas Hodge on 4/11/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
